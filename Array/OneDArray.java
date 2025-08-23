e322eeaa511079880547b720af843a4d71c12773
Nithish kumar k <nithish81812@gmail.com>
2025-08-23 20:06:50 +0530

    Adding 1DArray solution

---
+++ Array/OneDArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class OneDArray {
+ public int[] resultingSum(int[] nums) {
+   for(int i=1; i<nums.length ; i++) {
+      nums[i] += nums[i-1];
+   }
+   return nums;
+ }
+ public static void main(String[] args) {
+  OneDArray arr = new OneDArray();
+  int[] nums = {1,2,3,4,5};
+  int[] result = arr.resultingSum(nums);
+  System.out.println(Arrays.toString(result));
+  
+  
+  
+ }
+}
